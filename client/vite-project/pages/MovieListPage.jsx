f52503d63099cb5ca56a1058c8733144897b6304
Huy Hoang Nguyen <nguyenhhoang.hhn@gmail.com>
2025-08-01 23:38:36 -0400

    Fix

--- client/vite-project/pages/MovieListPage.jsx
+++ client/vite-project/pages/MovieListPage.jsx
@@ -8,6 +8,10 @@ function MovieListPage({getCookie}) {
   useEffect(() => {
     fetch("https://mighty-mesa-62871-571878c34ddf.herokuapp.com/api/movies", {
       method:"GET",
+      headers:{
+         'Content-Type': 'application/json',
+         'Authorization': `Bearer ${localStorage.getItem('token')}`
+      }
     }
     )
       .then((res) => res.json())
